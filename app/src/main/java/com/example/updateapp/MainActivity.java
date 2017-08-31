90a982adc217104778264d414f6c929881f2f3e3
13718134410 <1416817239@qq.com>
2017-08-31 16:17:25 +0800

    md

--- app/src/main/java/com/example/updateapp/MainActivity.java
+++ app/src/main/java/com/example/updateapp/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initModel() {
         model = new VersionModel();
-        model.forced = 1;
+        model.forced = 0;
         model.desc = "1:优化性能\n2:美化界面\n3:修复bug\n4:杀了一个产品祭天";
         model.url = "http://www.apk.anzhi.com/data3/apk/201703/14/4636d7fce23c9460587d602b9dc20714_88002100.apk";
         model.versionName = "测试demo";
